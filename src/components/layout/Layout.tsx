d90eff084a937444de927b6ef43d6bad53c610e8
gideon <kipkerring.gedion@gmail.com>
2025-05-07 17:16:41 +0300

    Layout.tsx

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-100">
+      <Navbar />
+      <main className="flex-1 container mx-auto px-4 py-8">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+export default Layout;
