5722d4fcf645bcaa34bfcde5ad74dfb34769a81d
gideon <kipkerring.gedion@gmail.com>
2025-05-07 16:56:13 +0300

    project clean up and tailwindcss setup and testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center text-xl bg-[#ECECEC] text-white">
+    <div className="min-h-screen flex justify-center items-center text-xl bg-[#4c4646] text-white">
       App
     </div>
   );
